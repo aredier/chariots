e97f9147c02abe53e879bfcc5823d9b947f25e08
aredier <antoine.redier2@gmail.com>
2019-02-19 23:01:00 +0000

    need to update more frequently

--- python/chariots/core/base_op.py
+++ python/chariots/core/base_op.py
@@ -2,8 +2,10 @@
 base op of chariots
 """
 
+import random
 from abc import ABC
 from abc import abstractmethod
+from abc import ABCMeta
 from typing import Optional
 from functools import partial
 
@@ -12,28 +14,45 @@ from chariots.core.dataset import DataSet
 from chariots.helpers.types import DataBatch
 
 
-class BaseOp(ABC):
+class AbstractOp(ABC):
     """
     base op of a pipeline
     the main entry point of the op is going to be the call method which will be perfomed on each data batch
     """
 
-    signature: Optional[Signature]
-    #TODO: infer this from signature
-    name = "paleceholder"
+    signature: Signature = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.signature is None:
+            raise ValueError(f"no signature was assigned to {cls.__name__}")
+        instance = super(AbstractOp, cls).__new__(cls, *args, **kwargs)
+        # instance.signature.add_fields(random_identifier = str(random.random() // 1e-16))
+        return instance
+
+    @property
+    def name(self):
+        return self.signature.name
     
+    @abstractmethod
+    def perform(self, dataset: DataSet, target = None) -> DataSet:
+        pass
+    
+
+class BaseOp(AbstractOp):
+
     @abstractmethod
     def _call(self, data_batch: DataBatch) -> DataBatch:
         pass
 
-    def perform(self, dataset: DataSet, target = None) -> DataSet:
+    def perform(self, dataset: DataSet, targets = None) -> DataSet:
         prev_meta = dataset.metadata
-        if target and target not in prev_meta.leafs:
-            raise ValueError(f"{target.name} not found in dataset")
+        targets = targets or prev_meta.leafs
         metadata = prev_meta.chain(self)
-        return DataSet.from_op(map(partial(self._perform_single, target=target), dataset), metadata)
+        return DataSet.from_op(map(partial(self._perform_single, targets=targets), dataset), prev_meta)
     
-    def _perform_single(self, data: DataBatch, target):
-        if target is None:
-           return {self.name: self._call(data[next(iter(data))])} 
-        return {self.name: self._call(data[target])}
+    def _perform_single(self, data: DataBatch, targets):
+        if len(targets) > 1:
+            print("foo")
+            return {self.name: self._call({key: value for key, value in data if key in targets})}
+        print
+        return {self.name: self._call(data[next(iter(data))])}
--- python/chariots/core/versioning.py
+++ python/chariots/core/versioning.py
@@ -1,5 +1,41 @@
+"""
+package that provides the signatures of each op
+"""
+
+import json
+from typing import Text
+from typing import Mapping
+from typing import Optional
+
+
 class Signature:
-    NotImplemented
+    """
+    a `Signature` represents an op, it identifies it from version to version
+    """
+    
+    def __init__(self, name: Text, identifiers: Optional[Mapping[Text, Text]] = None):
+        self.name = name
+        self._identifiers = identifiers or {}
+    
+    @property
+    def identifier(self):
+        return {"name": self.name, **self._identifiers}
+    
+    def add_fields(self, **kwargs):
+        self._identifiers = self._identifiers.update(kwargs)
+
+    @property
+    def checksum(self):
+        """
+        the checksum of an op
+        """
+        return hash(json.dumps(self.identifier))
+    
+    def matches(self, other: "Signature"):
+        return self.checksum == other.checksum
+
+    def __repr__(self):
+        return f"<Signature of {self.name}.{self.checksum}>"
 
 class VersionedSignature:
     NotImplemented
