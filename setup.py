778259cdb9c4ec938805b02a4b432d7f6b469e31
aredier <antoine.redier2@gmail.com>
2019-02-23 17:01:42 +0000

    adding setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chariots',
+      version='0.1.0',
+      description='machine learning versioned pipelines',
+      url='https://github.com/aredier/chariots',
+      author='Antoine Redier',
+      author_email='antoine.redier@hec.edu',
+      license='MIT',
+      packages=['chariots'],
+      zip_safe=False)
