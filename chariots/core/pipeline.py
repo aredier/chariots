5b14d9ebf7c1c22d0186dafd0e723525c6de049f
aredier <antoine.redier2@gmail.com>
2019-02-28 21:49:49 +0000

    first mockup of the trainable pipeline

--- chariots/core/pipeline.py
+++ chariots/core/pipeline.py
@@ -1,5 +1,7 @@
 from operator import attrgetter
 from typing import List
+from typing import Set
+from typing import Optional
 
 from chariots.core.ops import AbstractOp
 from chariots.core.metadata import Metadata
@@ -13,7 +15,9 @@ class Pipeline(AbstractOp):
     """
     signature = Signature("pipeline") 
 
-    def __init__(self, input_op: AbstractOp = None, output_op: AbstractOp = None):
+    def __init__(self, input_op: Optional[AbstractOp] = None, output_op: Optional[AbstractOp] = None):
+        if input_op is not None:
+            input_op.previous_op = None
         self.input_op = input_op
         self.output_op = output_op
 
@@ -31,6 +35,22 @@ class Pipeline(AbstractOp):
             self.output_op = other
         else:
             self.output_op = other(self.input_op)
+    
+    @property
+    def all_ops(self) -> Set[AbstractOp]:
+        if not isinstance(self.output_op, list):
+            all_ops = {self.output_op}
+        else:
+            all_ops = set(self.output_op)
+        queue = list(all_ops)
+        while queue:
+            op_of_interest = queue.pop()
+            all_ops.add(op_of_interest)
+            previous = op_of_interest.previous_op
+            if previous is not None:
+                if not isinstance(previous, list):
+                    previous = [previous]
+                queue.extend(previous)
 
     @classmethod
     def merge(cls, pipelines: List["Pipeline"]) -> "Pipeline":
