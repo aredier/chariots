42b9fa36d1150d959bee642533986aa51b516a43
aredier <antoine.redier2@gmail.com>
2019-03-01 21:08:22 +0000

    fixing pipeline tests

--- tests/test_markers.py
+++ tests/test_markers.py
@@ -9,36 +9,20 @@ from chariots.core.pipeline import Pipeline
 from chariots.core.ops import Split
 from chariots.core.markers import Number, Matrix
 
-class AddOneOp(BaseOp):
-    markers = [Number()]
-    requires = {"input_value": Number()}
-    signature = Signature(name = "add")
-
-    def _main(self, input_value):
-        return tap + 1
-
 class GenerateArray(BaseOp):
     markers = [Matrix((5,))]
     requires = {"input_value": Number()}
     signature = Signature(name="array_gen")
 
     def _main(self, input_value):
-        return [add for _ in range(5)]
-
-class Square(BaseOp):
-    markers = [Number()]
-    requires = {"input_value": Number()}
-    signature = Signature(name = "square")
-
-    def _main(self, input_value):
-        return tap ** 2
+        return [input_value for _ in range(5)]
 
 @pytest.fixture
 def tap():
     return DataTap(iter(range(10)), Number())
 
-def test_wrong_requires(tap):
-    add = AddOneOp()(tap)
+def test_wrong_requires(tap, add_op_cls, square_op_cls):
+    add = add_op_cls()(tap)
     array = GenerateArray()(add)
-    square = Square()
+    square = square_op_cls()
     square.when.called_with(array).should.throw(ValueError)
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -8,69 +8,43 @@ from chariots.core.taps import DataTap
 from chariots.core.pipeline import Pipeline
 from chariots.core.markers import Number
 
-class AddOneOp(BaseOp):
-    markers = [Number()]
-    requires = {"in_value": Number()}
-    signature = Signature(name = "add")
 
-    def _main(self, in_value):
-        return in_value + 1
-
-@pytest.fixture
-def add_op():
-    return AddOneOp()
-
-class Square(BaseOp):
-    markers = [Number()]
-    requires = {"in_value": Number()}
-    signature = Signature(name = "square")
-
-    def _main(self, in_value):
-        return in_value ** 2
-
-@pytest.fixture
-def square_op():
-    return Square()
-
-@pytest.fixture
-def tap():
-    return DataTap(iter(range(10)), Number())
-
-def test_single_op(add_op, tap):
-    add = add_op(tap)
+def test_single_op(add_op_cls, tap):
+    add = add_op_cls()(tap)
     for i, res in enumerate(add.perform()):
         res.should.be.a(dict)
-        res.should.have.key(add.markers[0]).being(i + 1)
+        res.should.have.key(add_op_cls.markers[0]).being(i + 1)
 
-def test_chained_op(tap, add_op, square_op):
-    add = add_op(tap)
-    square = square_op(add)
+def test_chained_op(tap, add_op_cls, square_op_cls):
+    add = add_op_cls()(tap)
+    square = square_op_cls()(add)
     for i, res in enumerate(square.perform()):
         res.should.be.a(dict)
-        res.should.have.key(square.markers[0]).being((i + 1) ** 2)
+        res.should.have.key(square_op_cls.markers[0]).being((i + 1) ** 2)
 
-def test_pipeline_add(tap, add_op, square_op):
+def test_pipeline_add(tap, add_op_cls, square_op_cls):
     pipe = Pipeline()
-    pipe.add(add_op)
-    pipe.add(square_op)
+    pipe.add(add_op_cls())
+    pipe.add(square_op_cls())
     for i, res in enumerate(pipe(tap).perform()):
         res.should.be.a(dict)
-        res.should.have.key(Square.markers[0]).being((i + 1) ** 2)
+        res.should.have.key(square_op_cls.markers[0]).being((i + 1) ** 2)
 
 
-def test_pipeline_init(tap, add_op, square_op):
-    add = add_op(tap)
-    square = square_op(add)
+def test_pipeline_init(tap, add_op_cls, square_op_cls):
+
+    add = add_op_cls()(tap)
+    square = square_op_cls()(add)
     pipe = Pipeline(tap, square)
     for i, res in enumerate(pipe(tap).perform()):
         res.should.be.a(dict)
-        res.should.have.key(square.markers[0]).being((i + 1) ** 2)
+        res.should.have.key(square_op_cls.markers[0]).being((i + 1) ** 2)
 
-def test_pipeline_as_an_op(tap, add_op, square_op):
-    add = add_op(tap)
+def test_pipeline_as_an_op(tap, add_op_cls, square_op_cls):
+    add = add_op_cls()(tap)
     pipe = Pipeline()
-    pipe.add(square_op)
+    pipe.add(square_op_cls())
     pipe(add)
     for i, res in enumerate(pipe.perform()):
         res.should.be.a(dict)
-        res.should.have.key(square_op.markers[0]).being((i + 1) ** 2)
+        res.should.have.key(square_op_cls.markers[0]).being((i + 1) ** 2)
