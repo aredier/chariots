0ccf5e13729a2211749848848b59e93f2db7da19
aredier <antoine.redier2@gmail.com>
2019-03-13 20:54:30 +0000

    adding more tests

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(name='chariots',
           'tests': ['pytest',
                     'sure',
                     'numpy',
-                    'sklearn'
+                    'sklearn',
+                    'pandas'
                    ],
       },
       zip_safe=False)
