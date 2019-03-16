c97cbbeaecc1917b4f82e7f41b1d63ac349940c5
aredier <antoine.redier2@gmail.com>
2019-03-16 17:30:19 +0000

    added test

--- chariots/core/dataset.py
+++ chariots/core/dataset.py
@@ -2,7 +2,6 @@
 from typing import Iterable
 from typing import Optional
 
-from chariots.core.metadata import Metadata
 
 ORIGIN = "original"
 
--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,3 +1,5 @@
+import numpy as np 
+
 from chariots.core.ops import Split
 from chariots.core.ops import Merge
 from chariots.core.ops import BaseOp
@@ -6,6 +8,7 @@ from chariots.training.evaluation import RegresionMetrics
 from chariots.core.requirements import Number
 from chariots.core.requirements import Matrix
 from chariots.core.requirements import FloatType
+from chariots.core.taps import DataTap
 
 YMarker = Matrix.create_child()
 YPred = Matrix.create_child() 
@@ -91,3 +94,39 @@ def test_regresion_metric_false(tap, add_op_cls):
     evaluation.should.have.key(str(res.version)).being.a(dict)
     evaluation[str(res.version)].should.have.key("mae").being.equal(1.)
     evaluation[str(res.version)].should.have.key("mse").being.equal(1.)
+
+def test_classification_metric_correct(tap, add_op_cls):
+    y_true, y_pred = Split(2)(tap)
+    y_true = XOp()(y_true)
+    y_pred = TrueY()(y_pred)
+    evaluation = ClassificationMetrics(XMarker.with_shape_and_dtype((1, 1), FloatType), 
+                                       YMarker.with_shape_and_dtype((1, 1), FloatType),
+                                       ["accuracy"])
+    res = Merge()([y_true, y_pred]) 
+    evaluation = evaluation.evaluate(res)
+    evaluation.should.be.a(dict)
+    evaluation.should.have.key(str(res.version)).being.a(dict)
+    evaluation[str(res.version)].should.have.key("accuracy").being.equal(1)
+
+def test_attaching_evaluaton(x_op_cls, linear_y_op_cls, linear_model_cls, x_requirement_cls):
+    numbers = np.random.choice(list(range(100)), 10, replace=True)
+
+    data = DataTap(iter(numbers), Number)
+    x = x_op_cls()(data)
+    x, y = Split(2)(x)
+    y = linear_y_op_cls()(y)
+    training_data =  Merge()([x, y])
+    foo = linear_model_cls()
+    foo.fit(training_data)
+    YTruReq = Matrix.create_child()
+    evaluation = RegresionMetrics(y_true=YTruReq, y_pred=foo.markers[0])
+    foo.attach_evaluation(evaluation)
+    x_test = DataTap(iter([i] for i in range(100)), x_requirement_cls)
+    y_true = DataTap(iter([i+1] for i in range(100)), YTruReq)
+    test_data = Merge()([x_test, y_true])
+    evaluation = foo.evaluate(test_data)
+    evaluation.should.be.a(dict)
+    evaluation.should.have.key(str(foo.version)).being.a(dict)
+    assert evaluation[str(foo.version)]["mae"] < 0.1
+    assert evaluation[str(foo.version)]["mse"] < 0.1
+
