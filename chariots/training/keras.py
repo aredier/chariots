377763b1de463bd0a228b9182a939d00cc7e60d8
aredier <antoine.redier2@gmail.com>
2019-03-18 21:13:55 +0000

    saving test and documentation

--- chariots/training/keras.py
+++ chariots/training/keras.py
@@ -2,6 +2,10 @@ import os
 from typing import Any
 from typing import Optional
 from typing import IO
+from typing import Callable
+from typing import Text
+from typing import Type
+from typing import Optional
 from tempfile import TemporaryDirectory
 
 from keras import models
@@ -11,6 +15,10 @@ from chariots.core import saving
 from chariots.training import trainable_op
 
 class KerasInput(requirements.Matrix):
+    """
+    base Requirement for KerasOp Inputs
+    """
+
 
     @classmethod
     def combine(cls, left: Any, right: Any) -> Any:
@@ -18,6 +26,9 @@ class KerasInput(requirements.Matrix):
 
 
 class KerasOutput(requirements.Matrix):
+    """
+    base Requirement for KerasOp outputs
+    """
 
     @classmethod
     def combine(cls, left: Any, right: Any) -> Any:
@@ -51,28 +62,47 @@ class KerasOp(saving.Savable, trainable_op.TrainableOp):
         with TemporaryDirectory() as temp_directory:
             transition_file = os.path.join(temp_directory, "model.h5")
             self._model.save(transition_file)
-            with open(transition_file, "r") as transition_file:
+            with open(transition_file, "rb") as transition_file:
                 temp_file.write(transition_file.read())
 
 
     @classmethod
     def _deserialize(cls, file: IO) -> "Savable":
         with TemporaryDirectory() as temp_directory:
-            transition_file = os.path.join(temp_directory, "model.h5")
-            with open(transition_file, "r") as transition_file:
-                transition_file.write(file.read)
-            model = models.load_model(transition_file)
+            transition_file_name = os.path.join(temp_directory, "model.h5")
+            with open(transition_file_name, "wb") as transition_file:
+                transition_file.write(file.read())
+            model = models.load_model(transition_file_name)
         res = cls()
         res._model = model
+        res._is_fited = True
         return res
     
     @classmethod
-    def factory(cls, build_function, name, doc="", inputs_marker = None, output_marker = None):
+    def factory(cls, build_function: Callable[[], models.Model], name: Text, doc: Text = "", 
+                inputs_requirements: Optional[Type[KerasInput]] = None, 
+                output_marker: Optional[Type[KerasOutput]] = None) -> Type["KerasOp"]:
+        """creates a KerasOp
+        Arguments:
+            build_function -- function to ccreate the underlying keras model
+            name -- the name of the op
+        
+        Keyword Arguments:
+            doc -- documentation to add to the Op
+            input_requirements -- the markers of the input of the underlying model, if None, these 
+                will be infered from `_inner_train` signature
+            output_markers -- the markers of the output of the underlying model, if None, these 
+                will be infered from signature
+        
+        Returns:
+            the resulting op
+        """
+
         res_cls = type(name, (cls,), {"__doc__": doc})
-        if inputs_marker is not None and output_marker is not None:
-            res_cls.training_requirements = {"model_input": inputs_marker, 
+        if inputs_requirements is not None and output_marker is not None:
+            res_cls.training_requirements = {"model_input": inputs_requirements, 
                                              "model_output": output_marker}
-            res_cls.requires = {"model_input": inputs_marker}
+            res_cls.requires = {"model_input": inputs_requirements}
             res_cls.markers = [output_marker]
         def _build_model(self):
             return build_function()
