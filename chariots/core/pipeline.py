3b49b5c18a83fcbe702cab59d16d0b65116a568a
aredier <antoine.redier2@gmail.com>
2019-03-23 18:43:53 +0000

    doc

--- chariots/core/pipeline.py
+++ chariots/core/pipeline.py
@@ -13,10 +13,26 @@ from chariots.training import TrainableTrait
 
 class Pipeline(TrainableTrait, AbstractOp):
     """
-    pipeline of operations that will perform mutliple poerations in a specific order
+    a pipeline represents a graph of operations and provides helper methods to perform on all 
+    relevant ops of the graph (saving, training, ...)
+    a pipeline is an op itself and can be used where a normal trainable op would have (even inside
+    another pipeline)
     """
 
-    def __init__(self, input_op: Optional[AbstractOp] = None, output_op: Optional[AbstractOp] = None):
+    def __init__(self, input_op: Optional[AbstractOp] = None,
+                 output_op: Optional[AbstractOp] = None):
+        """
+        both inputop and output op have to be set or non of them. if you want to create a piepline 
+        from one op and build on, use the add method on an empty pipeline
+
+        Keyword Arguments:
+            input_op  -- the input of the graph this pipeline represents (default: {None})
+            output_op -- the output of the graph this pipeline represents (default: {None})
+
+        Raises:
+            ValueError -- if only one of input and output op is set 
+        """
+
         self._op_graph = []
         if (input_op is None) != (output_op is None):
             raise ValueError("input_op and output op have either to be both set or None of them has"\
@@ -27,10 +43,45 @@ class Pipeline(TrainableTrait, AbstractOp):
             self._build_op_graph(self.input_op, self.output_op)
 
     @property
+    def ready(self):
+        return self.output_op.ready
+
+    @property
     def previous_op(self):
         return self.input_op.previous_op
 
-    def _build_op_graph(self, input_op, output_op):
+    @property
+    def all_ops(self) -> List[AbstractOp]:
+        """all the ops in the pipeline (order is deterministic but not guarenteed to be from input
+        ro output)
+
+        Returns:
+            list with all the ops
+        """
+
+        return [*[upstream for upstream, _ in self._op_graph], self.output_op]
+
+    @property
+    def fited(self):
+        """
+        whether or not the pipeline is fited
+        """
+        return self.output_op.ready
+
+
+    def _build_op_graph(self, input_op: AbstractOp, output_op: AbstractOp):
+        """
+        builds the internal op graph of the pipeline from an input and an output pipeline
+
+        Arguments:
+            input_op -- the input of the graph to build
+            output_op -- the output of the graph
+
+        Raises:
+            ValueError -- if the graph makes reference to ops beyond input op or if input op is not
+                connected to output op
+        """
+
         reading_heads = [output_op]
         while len(reading_heads):
             op_of_interest = reading_heads.pop()
@@ -50,14 +101,14 @@ class Pipeline(TrainableTrait, AbstractOp):
     def perform(self) -> DataSet:
         return self.output_op.perform()
 
-    def __call__(self, other: "AbstractOp") -> "AbstractOp":
+    def __call__(self, other: AbstractOp) -> AbstractOp:
         if self.input_op is not None:
             self.input_op(other)
         return self
 
     def add(self, other: AbstractOp, head=None):
         """
-        chains anoher op to the pipeline
+        adds another op to the pipeline after output op
         """
         self._link_versions(other)
         if self.input_op is None:
@@ -66,18 +117,6 @@ class Pipeline(TrainableTrait, AbstractOp):
         else:
             self._op_graph.append((self.output_op, other))
             self.output_op = other(self.output_op)
-
-    @property
-    def all_ops(self) -> Set[AbstractOp]:
-        return [*[upstream for upstream, _ in self._op_graph], self.output_op]
-
-    @property
-    def fited(self):
-        """
-        whether or not the pipeline is fited
-        """
-        return self.output_op.ready
-
     def fit(self, other: Optional[AbstractOp] = None, mode: Text = "naive"):
         """
         fiting the pipeline
@@ -105,11 +144,23 @@ class Pipeline(TrainableTrait, AbstractOp):
             self.input_op.previous_op = None
 
     def save(self, saver: Saver):
+        """saves all the ops inside the pipeline
+
+        Arguments:
+            saver -- the saver to handle the saving
+        """
+
         for op in self.all_ops:
             if isinstance(op, Savable):
                 op.save(saver)
 
-    def load(self, saver):
+    def load(self, saver: Saver):
+        """loads the ops in the graph that have been persisted
+
+        Arguments:
+            saver -- the saver to handel io
+        """
+
         unloaded_ops = self.all_ops
         while len(unloaded_ops):
             op_of_interest = unloaded_ops.pop(0)
@@ -122,9 +173,9 @@ class Pipeline(TrainableTrait, AbstractOp):
             print("fited at loading", loaded_op.fited)
             self._replace_op_in_graph(op_of_interest, loaded_op)
 
-    def _replace_op_in_graph(self, op_to_replace, replacing_op):
-        print("fited before replacing", replacing_op.fited)
-        print(any((up == op_to_replace for _, up in self._op_graph)))
+    def _replace_op_in_graph(self, op_to_replace: AbstractOp, replacing_op: AbstractOp):
+        """replaces an op in the graph with another"""
+
         res_op_graph = []
         for downstream, upstream in self._op_graph:
             downstream = replacing_op if downstream == op_to_replace else downstream
@@ -133,16 +184,7 @@ class Pipeline(TrainableTrait, AbstractOp):
             res_op_graph.append((upstream, downstream))
         self._op_graph = res_op_graph
 
-        print("fited after replacing", replacing_op.fited)
         if op_to_replace == self.input_op:
             self.input_op = replacing_op
         if op_to_replace == self.output_op:
-            print("got you")
             self.output_op = replacing_op
-            print(self.output_op.fited)
-        print("some left", any((up == op_to_replace for _, up in self._op_graph)))
-        print("some replaced", any((up == replacing_op for _, up in self._op_graph)))
-
-    @property
-    def ready(self):
-        return self.output_op.ready
