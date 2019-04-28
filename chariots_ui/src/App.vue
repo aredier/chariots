6bc7105ecd40d72123ee9c9ba1b3bbc73b45bf78
aredier <antoine.redier2@gmail.com>
2019-04-28 19:39:20 +0100

    proto routiong and store dome

--- chariots_ui/src/App.vue
+++ chariots_ui/src/App.vue
@@ -8,50 +8,27 @@
       app
     >
       <v-list dense>
-        <v-list-tile @click="">
+        <v-list-tile  v-for="series in this.seriesName" @click="pushRoute('/' +series)">
           <v-list-tile-action>
-            <v-icon>dashboard</v-icon>
+            <v-icon >trending_up</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Dashboard</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon>settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Settings</v-list-tile-title>
+            <v-list-tile-title>{{series}}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app fixed clipped-left>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Chariots Ui</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat @click.stop="fetchData"><v-icon>cached</v-icon></v-btn>
+      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <v-container fluid fill-height>
-        <v-layout justify-center align-center>
-          <v-flex shrink>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>code</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn icon large href="https://codepen.io/johnjleider/pen/qxQWda" target="_blank" v-on="on">
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-flex>
-        </v-layout>
+         <router-view></router-view>
       </v-container>
     </v-content>
     <v-footer app fixed>
@@ -61,12 +38,24 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld';
+import {mapGetters, mapActions} from 'vuex';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  computed:{
+    ...mapGetters([
+      "seriesName",
+    ])
+  },
+  methods: {
+    ...mapActions(["fetchData"]),
+    pushRoute(route) {
+      this.$router.push(route)
+    }
   },
   data: () => ({
       drawer: null
---
+++ chariots_ui/src/views/DataPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>This is not yet a data page for {{this.series_name}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    series_name: "NA"
+  }),
+  watch: {
+    '$route' (to, from) {
+      this.series_name = to.params.series_name
+    }
+  }
+}
+</script>
+
