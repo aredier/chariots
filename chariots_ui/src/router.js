a3c277bb7fa8d573c2e1b422ef185f218cd262ee
aredier <antoine.redier2@gmail.com>
2019-04-28 19:39:20 +0100

    proto routiong and store dome

--- chariots_ui/src/router.js
+++ chariots_ui/src/router.js
@@ -1,23 +1,18 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import DataPage from './views/DataPage.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
+      path: '/:series_name',
+      name: 'series data',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: DataPage,
     },
   ],
 });
--- chariots_ui/src/store.js
+++ chariots_ui/src/store.js
@@ -5,12 +5,39 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    series: [
+      {
+        "seriesName": "foo",
+        "point": []
+      }
+    ],
+    loaded: false
 
   },
+  getters: {
+    seriesName: state => state.series.map(el => el.seriesName),
+  },
   mutations: {
+    SetApplicationData(state, new_series) {
+      state.series = new_series;
+      state.loaded = true;
+    },
+    Loading(state) {
+      state.loaded = false;
+    }
 
   },
   actions: {
+    async fetchData({state, commit}) {
+      commit("Loading");
+      let data = [
+        {
+          "seriesName": "perf",
+          "point": []
+        }
+      ];
+      commit("SetApplicationData", data);
+    }
 
   },
 });
