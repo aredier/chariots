7ce1b1b495c0f386740147aac4b173842a8f570b
aredier <aredier@MacBook-Pro-de-aredier.local>
2019-06-26 19:51:47 +0100

    more test

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'dill']
+requirements = ['Click>=6.0', 'dill==0.2.9']
 
 setup_requirements = ['pytest-runner', ]
 
