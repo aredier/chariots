c07a95443097aa3a67093ccc85744b797e711fb7
aredier <antoine.redier2@gmail.com>
2020-02-06 17:07:20 +0100

    small cleaning

--- chariots/workers/_base_worker_pool.py
+++ chariots/workers/_base_worker_pool.py
@@ -8,6 +8,9 @@ import chariots
 
 
 class JobStatus(Enum):
+    """
+    enum of all the possible states a job can be in.
+    """
     queued = 'queued'
     running = 'running'
     done = 'done'
@@ -15,7 +18,10 @@ class JobStatus(Enum):
     deferred = 'deferred'
 
     @classmethod
-    def from_rq(cls, status):
+    def from_rq(cls, status: rq.job.JobStatus) -> 'JobStatus':
+        """
+        Translates an RQ Job status into a Chariots JobStatus
+        """
         if status == rq.job.JobStatus.QUEUED:
             return cls.queued
         if status == rq.job.JobStatus.FINISHED:
@@ -30,19 +36,39 @@ class JobStatus(Enum):
 
 
 class JobResponse(ABC):
+    """
+    Response output produced once a Job. You can get the `status` and the `result` (when available)
+    of the job.
+    """
 
     @property
     @abstractmethod
     def status(self) -> JobStatus:
+        """
+        the status of the Job in question.
+        """
         pass
 
     @property
     @abstractmethod
     def result(self) -> Any:
+        """
+        the results of the Job at hand.
+        """
         pass
 
 
 class BaseWorkerPool(ABC):
+    """
+    `BaseWorkerPool` is the class you will need to subclass in order to make your own JobQueue system
+    work with Chariots.
+
+    In order to do so you will need to create:
+    - n_workers: a property that informs Chariots of the total number of workers (available as well as in use)
+    - spawn_worker: a method that creates a new worker
+    - execute_pipeline_async: a method that executes a pipeline inside one of this Pool's workers.
+    - get_pipeline_response_json_for_id: a method that is available to retreieve
+    """
 
     @property
     @abstractmethod
