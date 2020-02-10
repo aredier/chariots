27c0117686e6109865040e2b7d3ca4001b3b303b
aredier <antoine.redier2@gmail.com>
2020-02-10 18:17:55 +0100

    using tox for linting rather than travis

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-with open('./setup_requirements.txt') as requirements_file:
+with open('setup_requirements.txt') as requirements_file:
     requirements = list(requirements_file.readlines())
 
 setup_requirements = ['pytest-runner', ]
