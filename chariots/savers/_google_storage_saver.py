06af74ccda6eecea839f3c29dc3b21d1a0984ff2
aredier <antoine.redier2@gmail.com>
2020-03-06 20:01:04 +0100

    more serialisation problems

--- chariots/savers/_google_storage_saver.py
+++ chariots/savers/_google_storage_saver.py
@@ -14,11 +14,14 @@ class GoogleStorageSaver(BaseSaver):
     :param bucket: a google.could.storage `Bucket` object to save the data to
     """
 
-    def __init__(self, root_path: Text, bucket: storage.bucket.Bucket):
+    def __init__(self, root_path: Text, bucket_name: Text, client_kwargs: Optional[Mapping] = None):
         """
         """
         super().__init__(root_path)
-        self.bucket = bucket
+        self._client_kwargs = client_kwargs or {}
+        self._bucket_name = bucket_name
+        storage_client = storage.Client(**self._client_kwargs)
+        self.bucket = storage_client.bucket(self._bucket_name)
 
     def save(self, serialized_object: bytes, path: Text) -> bool:
         blob = self.bucket.blob(path)
@@ -29,3 +32,12 @@ class GoogleStorageSaver(BaseSaver):
         if blob is None:
             raise FileNotFoundError('{} does not exist'.format(path))
         return blob.download_as_string()
+
+    def __getstate__(self):
+        self.bucket = None
+        return self.__dict__
+
+    def __setstate__(self, state):
+        self.__dict__ = state
+        storage_client = storage.Client(**self._client_kwargs)
+        self.bucket = storage_client.bucket(self._bucket_name)
